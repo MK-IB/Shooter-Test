4a67e844249a58b50f322f7c5a75081e64ac1d40
Mallik Ebadat <31974569+MK-IB@users.noreply.github.com>
2025-01-17 15:15:06 +0530

    Splash Screen +More Polishing

--- Assets/_Shooter/_Scripts/ControllerRelated/UIGame.cs
+++ Assets/_Shooter/_Scripts/ControllerRelated/UIGame.cs
@@ -9,9 +9,17 @@ using UnityEngine.UI;
 public class UIGame : MonoBehaviour
 {
     [SerializeField] private Button shootButton;
+    [SerializeField] private GameObject splashScreen;
 
     private void Start()
     {
         shootButton.onClick.AddListener(Game.On_ShootButtonPressed);
+        StartCoroutine(DisableSplashScreen());
+    }
+
+    IEnumerator DisableSplashScreen()
+    {
+        yield return new WaitForSeconds(2);
+        splashScreen.SetActive(false);
     }
 }
